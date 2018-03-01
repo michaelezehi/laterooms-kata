826b6ca5dcbb75f97f6a5f741189be74f0da23c7
Tom Howell <tom@instig8.co.uk>
2018-03-01 10:08:22 +0000

    HotelFilter test adding and removal of filter value

--- src/HotelFilter/index.test.js
+++ src/HotelFilter/index.test.js
@@ -29,11 +29,41 @@ const filterResults = () => {
 it('renders without crashing', () => {
   const component = renderer.create(
     <HotelFilter
-      filters={[]}
+      filters={FILTERS}
       updateFilters={updateFilters}
       filterResults={filterResults}
     />,
   );
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+it('adds filter to filters arr', () => {
+  const component = shallow(
+    <HotelFilter
+      filters={FILTERS}
+      updateFilters={updateFilters}
+      filterResults={filterResults}
+    />,
+  );
+
+  component.instance().addFilter('car park');
+  expect(FILTERS[0]).toBe('car park');
+  expect(FILTERS.length).toBe(1);
+});
+
+it('adds removes filter from filters arr', () => {
+  const component = shallow(
+    <HotelFilter
+      filters={FILTERS}
+      updateFilters={updateFilters}
+      filterResults={filterResults}
+    />,
+  );
+
+  expect(FILTERS[0]).toBe('car park');
+  expect(FILTERS.length).toBe(1);
+
+  component.instance().removeFilter('car park');
+  expect(FILTERS.length).toBe(0);
 });
