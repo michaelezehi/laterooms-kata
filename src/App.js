f7e9576f60dc7a6bba22b3a7f31afdb55875e995
Tom Howell <tom@instig8.co.uk>
2018-03-01 10:09:14 +0000

    Add HotelFilter into application

--- src/App.js
+++ src/App.js
@@ -1,16 +1,49 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import { FormGroup, Checkbox } from 'react-bootstrap';
-
 import { HOTELS } from './hotelData';
+import HotelFilter from './HotelFilter/';
 import HotelList from './HotelList/';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      hotelList: HOTELS,
+      filters: [],
+    }
+  }
+
+  updateFilters(val) {
+    this.setState({ filters: val });
+  }
+
+  filterResults() {
+
+    let filteredResults = [];
+
+    if (this.state.filters.length > 0) {
+      filteredResults = HOTELS.filter(h => {
+        return h.Facilities.some(v => (this.state.filters.indexOf(v) >= 0 ));
+      });
+    } else {
+      filteredResults = HOTELS;
+    }
+
+    this.setState({ hotelList: filteredResults });
+  }
+
   render() {
     return (
       <div className="App">
-        <HotelList listHotels={HOTELS} />
+      <HotelFilter
+          filters={this.state.filters}
+          updateFilters={this.updateFilters.bind(this)}
+          filterResults={this.filterResults.bind(this)}
+        />
+        <br /><br />
+        <HotelList listHotels={this.state.hotelList} />
       </div>
     );
   }
